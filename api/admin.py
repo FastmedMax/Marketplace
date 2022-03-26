21ce6ac703462565d6bf6b273c2368bebfac5b03
fastmed <maks.memelige@gmail.com>
2022-03-26 10:00:50 +0200

    Add admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import User, UserProduct
+
+
 # Register your models here.
+admin.site.register(UserProduct)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = (
+        "password", "groups", "is_active",
+        "is_staff", "is_superuser", "last_login",
+        "date_joined", "user_permissions"
+        )

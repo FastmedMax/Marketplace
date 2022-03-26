0eb52bff31d979fe930a7bc0b2fb2231b2c3ff2c
fastmed <maks.memelige@gmail.com>
2022-03-26 09:54:10 +0200

    Add imports for views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+
+from .models import User, Product, ProductRate, UserProduct
+
+from .serializers import UserSerializer, ProductSerializer, ProductRateSerializer, UserProductSerializer
+
 
 # Create your views here.

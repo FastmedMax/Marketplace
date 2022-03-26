985a5e438f0810f35b355e6ebe5f7e75fa50ca29
fastmed <maks.memelige@gmail.com>
2022-03-26 09:50:41 +0200

    Add User model

--- api/models.py
+++ api/models.py
@@ -5,4 +5,21 @@ from django.contrib.auth.models import AbstractUser
 
 from .managers import UserManager
 
+
 # Create your models here.
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        verbose_name="Почта",
+        unique=True,
+        validators=[validators.validate_email],
+        error_messages={
+            "unique": "Пользователь с таким email уже существует.",
+        },)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ()
+
+    objects = UserManager()
+

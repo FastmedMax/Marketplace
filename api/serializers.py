6a20328bbf335173318431d6279808b0eda10548
fastmed <maks.memelige@gmail.com>
2022-03-26 09:52:58 +0200

    Add ProductSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -7,3 +7,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"

1fcb7aebe364bc66194f9b232fd6ecb0208cad68
fastmed <maks.memelige@gmail.com>
2022-03-26 09:51:35 +0200

    Add Product model

--- api/models.py
+++ api/models.py
@@ -23,3 +23,14 @@ class User(AbstractUser):
 
     objects = UserManager()
 
+
+class Product(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=60)
+    description = models.TextField(verbose_name="Описание")
+    short_description = models.CharField(verbose_name="Короткое описание", max_length=100)
+    image = models.ImageField(verbose_name="Картинка")
+    file = models.FileField(verbose_name="Файл")
+    downloads = models.PositiveBigIntegerField(verbose_name="Скачивания")
+    price = models.FloatField(verbose_name="Цена")
+    is_free = models.BooleanField(verbose_name="Бесплатно ли")
+

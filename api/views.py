9935093a68f2af6c66a3821eac6bb7586a17f3a1
fastmed <maks.memelige@gmail.com>
2022-03-26 09:57:47 +0200

    Add ProductRateViewSet

--- api/views.py
+++ api/views.py
@@ -16,3 +16,16 @@ class ProductViewSet(viewsets.GenericViewSet):
         products = self.queryset.values_list("title", "description", "short_description", "image", "downloads", "file")
         return Response(products, status=status.HTTP_200_OK)
 
+
+class ProductRateViewSet(viewsets.GenericViewSet):
+    queryset = ProductRate
+    serializer_class = ProductRateSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid(raise_exception=False):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

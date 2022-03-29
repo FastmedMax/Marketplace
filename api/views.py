63f6b7aa3eb8346642c8170b64b53414d9e38cee
fastmed <maks.memelige@gmail.com>
2022-03-29 13:21:57 +0200

    Add imports for api.views

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
 from .models import User, Product, ProductRate, UserProduct
 

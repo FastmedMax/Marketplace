a3b077629337271ed73c1e19d2f82b39c9e24c18
fastmed <maks.memelige@gmail.com>
2022-04-05 12:00:38 +0200

    Update urls

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet, ProductRateViewSet
+from .views import ProductViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
-router.register(r"product", ProductViewSet, basename="product")
-router.register(r"rate", ProductRateViewSet, basename="product_rate")
+router.register(r"products", ProductViewSet, basename="products")
 
 urlpatterns = [
 ] + router.urls

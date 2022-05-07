84b9080e2baab934d1c556ddb42d77d199549f0f
S0ltero <AlexsSobko61@gmail.com>
2022-05-07 17:42:51 +0300

    Change paths for `static` and `media` files

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
 }
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = []
 STATIC_URL = "/static/"
 STATICFILES_FINDERS = (
@@ -71,7 +71,7 @@ STATICFILES_FINDERS = (
 )
 
 # Media files
-MEDIA_ROOT = join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 MEDIA_URL = "/media/"
 
 TEMPLATES = [

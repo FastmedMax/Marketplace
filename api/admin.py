6d16400b8c7f3b7c89d9eca9a51888066a5e424f
S0ltero <AlexsSobko61@gmail.com>
2022-05-17 16:10:07 +0300

    Import new models

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, UserProduct, Product, ProductRate
+from .models import User, Category, UserProduct, Product, ProductImage, ProductRate
 
 
 @admin.register(User)
